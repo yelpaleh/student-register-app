3e22aac3e7852cbe198a7a6497a8acabd1e97902
Harshal Yelpale <harshal.yelpale@gmail.com>
2025-03-18 10:05:15 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'student-register-app';
+  student = {
+    fullName: '',
+    email: '',
+    dob: '',
+    gender: '',
+    course: ''
+  };
+
+  courses: string[] = ['Computer Science', 'Mathematics', 'Physics', 'Chemistry'];
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form Data:', this.student);
+      alert('Registration Successful!');
+
+      // Reset the form after submission
+      form.reset();
+    }
+  }
 }
